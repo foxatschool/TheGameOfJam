c3ec3957020d09ee2fa5e49a427ee752c279fa2f
foxatschool <kfox@student.neumont.edu>
2026-02-28 23:02:48 -0700

    made improvements to destructionbase

--- NotEnoughParts/Assets/Game/Scripts/DestructionBase.cs
+++ NotEnoughParts/Assets/Game/Scripts/DestructionBase.cs
@@ -1,17 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public abstract class DestructionBase : MonoBehaviour
+public class DestructionBase : MonoBehaviour
 {
+    [SerializeField] protected string objectName = "Destructible Object";
     [SerializeField] protected float degradationRate = 0.1f;
     [SerializeField] protected float repairRate = 0.1f;
     [SerializeField] protected float maxHealth = 100f;
     [SerializeField] protected float currentHealth = 100f;
     [SerializeField] protected bool raparing = false;
-    [SerializeField] protected bool isDestroyed = false;
     [SerializeField] protected float maxRandom = 1f;
+    [SerializeField] protected AudioClip[] audioClips = new AudioClip[6];
     private void Update()
     {
-        if (!raparing && !isDestroyed)
+        if (!raparing && currentHealth <= 0)
         {
             float randomFactor = Random.Range(0f, maxRandom);
             currentHealth -= degradationRate * Time.deltaTime * randomFactor;
@@ -21,5 +24,18 @@ public abstract class DestructionBase : MonoBehaviour
             currentHealth += repairRate * Time.deltaTime;
         }
 
+        if(currentHealth < 75 && currentHealth > 73)
+        {
+            //audioClips[0].PlayOneShot();
+        }
+
     }
+
+    public float getCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    
+
 }
