8c74d6238367473105e30a650caacbb396d18973
LLCardenas-117 <lcardenas@student.neumont.edu>
2026-03-02 11:26:49 -0700

    Added the panels to the character died

--- NotEnoughParts/Assets/Game/Scripts/CharacterDied.cs
+++ NotEnoughParts/Assets/Game/Scripts/CharacterDied.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class CharacterDied : MonoBehaviour
 {
+    [SerializeField] private GameObject playerPanel;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject LosePanel;
+
     public void Die()
     {
         Time.timeScale = 0f;
+        playerPanel.SetActive(false);
+        pausePanel.SetActive(false);
+        LosePanel.SetActive(true);
     }
 }
